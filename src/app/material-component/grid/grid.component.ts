8efb8eac2cb8f70777f4ce7ca9c684e17811528b
unknown <test@test.com>
2020-01-20 08:37:34 +0530

    New Commit

--- src/app/material-component/grid/grid.component.ts
+++ src/app/material-component/grid/grid.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
--- src/app/material-component/lists/lists.component.ts
+++ src/app/material-component/lists/lists.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+export interface User {
+  id: string,
+  value: string;
+}
+
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
   styleUrls: ['./lists.component.scss']
 })
-export class ListsComponent {
+
+
+export class ListsComponent implements OnInit{
+
+  public form: FormGroup;
+  options1: User[] = [{ id: '1', value: 'ABC1' },{ id: '2', value: 'ABC2' }, { id: '3', value: 'ABC3' }, { id: '4', value: 'ABC4' }, { id: '5', value: 'ABC5' }];
+  
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      ip1: [
+        "ABC",
+        Validators.compose([Validators.required])
+      ],
+      ip2: [
+        null,
+        Validators.compose([Validators.required])
+      ],
+      ip3: [
+        null,
+        Validators.compose([Validators.required])
+      ],
+      ip4: [
+        null,
+        Validators.compose([Validators.required])
+      ],
+      ip5: [
+        null,
+        Validators.compose([Validators.required])
+      ],
+      ip6: [
+        null,
+        Validators.compose([Validators.required])
+      ],
+      date1: [
+        null,
+        Validators.compose([Validators.required])
+      ],
+      date2: [
+        null,
+        Validators.compose([Validators.required])
+      ],
+      myControl1:[null,Validators.required],
+      myControl2:[null,Validators.required],
+      myControl3:[null,Validators.required],
+      myControl4:[null,Validators.required],
+      checkbox:[null]
+    });
+
+  
+  }
+
+  
   
 }
